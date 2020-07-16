948ad712a8e67abbfbe4efa2991b42e92e05f987
Nirbhay-Thacker <67470607+Nirbhay-Thacker@users.noreply.github.com>
2020-07-16 22:36:16 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Acknowledgements to Dhaval Bothra for introducing me to the Karatsuba Algorithm and to Jinay Shah for helping me debug, identifying sub_digits as the cause of errors in operator* and suggesting a fix.
 #include"num.hpp"
 #include <iostream>
 
